c93a9e7da34c2090bc7e5bbe725407c57fa4456c
SamuelOmodele <samuelomodele19@gmail.com>
2025-09-27 17:47:09 +0100

    chore: added select field in reports page

--- app/admin/reports/page.tsx
+++ app/admin/reports/page.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './reports.module.css'
+import { appIcons } from '@/app/assets/icons/icons'
+import IncidentModal from '@/app/components/incidentModal/incidentModal'
 
 const reportHistory = [
     { id: 1, category: 'Theft', description: 'Theves broke in a house and stole valuables', severity: 'High', location: 'Agbowo' },
@@ -14,9 +17,38 @@ const reportHistory = [
 
 const ReportsPage = () => {
 
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <div className={styles.reportHistory} >
-            <h3>Report History</h3>
+            <div className={styles.header}>
+                <h3>Report History</h3>
+
+                <div className={styles.selectContainer}>
+                    <div>
+                        <appIcons.dropdownIcon className={styles.icon} size={24} />
+                        <select name="category" id="category">
+                            <option value="all">All category</option>
+                            <option value="all">Theft</option>
+                        </select>
+                    </div>
+                    <div>
+                        <appIcons.dropdownIcon className={styles.icon} size={24} />
+                        <select name="severity" id="severity">
+                            <option value="all">All severity</option>
+                            <option value="all">low</option>
+                            <option value="all">moderate</option>
+                            <option value="all">high</option>
+                            <option value="all">critcal</option>
+                        </select>
+                    </div>
+
+                </div>
+            </div>
+
             <table>
                 <thead>
                     <tr className={styles.head}>
@@ -29,7 +61,7 @@ const ReportsPage = () => {
                 </thead>
                 <tbody>
                     {reportHistory.map((report) => (
-                        <tr key={report.id}>
+                        <tr key={report.id} onClick={handleOpen}>
                             <td>{report.id}</td>
                             <td>{report.category}</td>
                             <td>{report.description}</td>
@@ -39,7 +71,9 @@ const ReportsPage = () => {
                     ))}
                 </tbody>
             </table>
-        </div >
+
+            <IncidentModal open={open} handleClose={handleClose} />
+        </div>
     )
 }
 
